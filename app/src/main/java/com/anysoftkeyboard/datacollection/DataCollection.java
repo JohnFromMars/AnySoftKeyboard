5a35120806eb8f08b271a393c2455f75420dfefd
JohnFromMars <rock13810@gmail.com>
2019-07-08 14:54:06 +0800

    New commit for the data collection class and the correlative test

---
+++ app/src/main/java/com/anysoftkeyboard/datacollection/DataCollection.java
@@ -0,0 +1,8 @@
+package com.anysoftkeyboard.datacollection;
+
+/**
+ *
+ */
+public class DataCollection {
+    private String test;
+}
---
+++ app/src/test/java/com/anysoftkeyboard/datacollection/DataCollectionTest.java
@@ -0,0 +1,15 @@
+package com.anysoftkeyboard.datacollection;
+
+import com.anysoftkeyboard.AnySoftKeyboardRobolectricTestRunner;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+@RunWith(AnySoftKeyboardRobolectricTestRunner.class)
+public class DataCollectionTest {
+}
