0114df99253e35633264a655600ddec1d5408453
JohnFromMars <rock13810@gmail.com>
2019-08-18 12:08:20 +0800

    Data collection Keystroke - add Keystroke functions in DataCollection class

--- app/src/main/java/com/anysoftkeyboard/datacollection/DataCollection.java
+++ app/src/main/java/com/anysoftkeyboard/datacollection/DataCollection.java
@@ -11,14 +11,24 @@ public class DataCollection {
     private String startPoint;
     private String endPoint;
     private ArrayList<Word> words;
+    private ArrayList<Keystroke> keystrokes;
 
 
     public DataCollection() {
         words = new ArrayList<>();
+        keystrokes = new ArrayList<>();
     }
 
     public void addWord(Word word) {
         this.words.add(word);
+    }
+
+    public void addKeystroke(Keystroke keystroke) {
+        this.keystrokes.add(keystroke);
+    }
+
+    public Keystroke getLastKeystroke() {
+        return keystrokes.get(this.keystrokes.size() - 1);
     }
 
     public ArrayList<Word> getWords() {
@@ -27,5 +37,13 @@ public class DataCollection {
 
     public void setWords(ArrayList<Word> words) {
         this.words = words;
+    }
+
+    public ArrayList<Keystroke> getKeystrokes() {
+        return keystrokes;
+    }
+
+    public void setKeystrokes(ArrayList<Keystroke> keystrokes) {
+        this.keystrokes = keystrokes;
     }
 }
