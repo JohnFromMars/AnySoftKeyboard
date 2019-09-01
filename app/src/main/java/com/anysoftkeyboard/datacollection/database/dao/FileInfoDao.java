2973e9bce555d65ff0b9702d1e59303646a07352
JohnFromMars <rock13810@gmail.com>
2019-09-01 11:29:00 +0800

    Data collection dataTransmitter new findAllFileInfo method

--- app/src/main/java/com/anysoftkeyboard/datacollection/database/dao/FileInfoDao.java
+++ app/src/main/java/com/anysoftkeyboard/datacollection/database/dao/FileInfoDao.java
@@ -8,6 +8,8 @@ import android.database.sqlite.SQLiteDatabase;
 import com.anysoftkeyboard.base.utils.Logger;
 import com.anysoftkeyboard.datacollection.database.model.FileInfo;
 
+import java.util.ArrayList;
+
 /**
  * This class is database access object of file_info table in the data collection database
  */
@@ -67,37 +69,68 @@ public class FileInfoDao {
      * Query the FileInfo according to the given file name.
      * Return null if there is no record found in the database
      *
-     * @param fileName file name
+     * @param fileInfo file information
      * @return FileInfo Object for the data collection database
      */
-    public FileInfo getFileInfoByFileName(String fileName) {
-        FileInfo fileInfo = null;
+    public FileInfo findFileInfo(FileInfo fileInfo) {
+        FileInfo resultFileInfo = null;
 
-        Cursor result = database.rawQuery("select * from " + FileInfo.FILE_INFO_TABLE_NAME + " where " + FileInfo.FILE_INFO_FILE_NAME + " = ?", new String[]{fileName});
+        Cursor result = database.rawQuery("select * from " + FileInfo.FILE_INFO_TABLE_NAME + " where " + FileInfo.FILE_INFO_FILE_NAME + " = ?", new String[]{fileInfo.getFileName()});
         if (result != null && result.getCount() > 0) {
             result.moveToFirst();
-            fileInfo = new FileInfo(result.getString(result.getColumnIndex(FileInfo.FILE_INFO_FILE_NAME)));
-            fileInfo.setId(result.getInt(result.getColumnIndex(FileInfo.FILE_INFO_ID)));
-            fileInfo.setHasSent(result.getInt(result.getColumnIndex(FileInfo.FILE_INFO_HAS_SENT)) > 0);
-            fileInfo.setSize(result.getInt(result.getColumnIndex(FileInfo.FILE_INFO_SIZE)));
+            resultFileInfo = new FileInfo(result.getString(result.getColumnIndex(FileInfo.FILE_INFO_FILE_NAME)));
+            resultFileInfo.setId(result.getInt(result.getColumnIndex(FileInfo.FILE_INFO_ID)));
+            resultFileInfo.setHasSent(result.getInt(result.getColumnIndex(FileInfo.FILE_INFO_HAS_SENT)) > 0);
+            resultFileInfo.setSize(result.getInt(result.getColumnIndex(FileInfo.FILE_INFO_SIZE)));
         }
 
-        if(result != null){
+        if (result != null) {
             result.close();
         }
 
         Logger.d("test", "dc-- DataTransmitter getFile finish");
-        return fileInfo;
+        return resultFileInfo;
     }
 
     /**
      * delete the FileInfo record in the database according to the given file name
      *
-     * @param fileName file name
+     * @param fileInfo file information
      */
-    public void deleteFileInfoByFileName(String fileName) {
-        database.delete(FileInfo.FILE_INFO_TABLE_NAME, FileInfo.FILE_INFO_FILE_NAME + " = ?", new String[]{fileName});
+    public void deleteFileInfo(FileInfo fileInfo) {
+        database.delete(FileInfo.FILE_INFO_TABLE_NAME, FileInfo.FILE_INFO_FILE_NAME + " = ?", new String[]{fileInfo.getFileName()});
         Logger.d("test", "dc-- DataTransmitter finish delete");
     }
 
+
+    /**
+     * Get all files in the database according to hasSent attribute
+     *
+     * @param hasSent true or false
+     * @return all  files information
+     */
+    public ArrayList<FileInfo> findAllFileInfoByHasSent(boolean hasSent) {
+        ArrayList<FileInfo> fileInfos = new ArrayList<>();
+
+        Cursor result = database.rawQuery("select * from " + FileInfo.FILE_INFO_TABLE_NAME + " where " + FileInfo.FILE_INFO_HAS_SENT + " = ?", new String[]{hasSent ? "1" : "0"});
+
+        if (result != null && result.getCount() > 0) {
+            result.moveToFirst();
+
+            do {
+                FileInfo fileInfo = new FileInfo(result.getString(result.getColumnIndex(FileInfo.FILE_INFO_FILE_NAME)));
+                fileInfo.setId(result.getInt(result.getColumnIndex(FileInfo.FILE_INFO_ID)));
+                fileInfo.setHasSent(result.getInt(result.getColumnIndex(FileInfo.FILE_INFO_HAS_SENT)) > 0);
+                fileInfo.setSize(result.getInt(result.getColumnIndex(FileInfo.FILE_INFO_SIZE)));
+                fileInfos.add(fileInfo);
+
+            } while (result.moveToNext());
+        }
+
+        if (result != null) {
+            result.close();
+        }
+
+        return fileInfos;
+    }
 }
