db6035a7f2337e9000704b773d21ff465fd08451
JohnFromMars <rock13810@gmail.com>
2019-10-09 08:25:01 +0800

    Data collection battery info

--- app/src/main/java/com/anysoftkeyboard/datacollection/BatteryInfo.java
+++ app/src/main/java/com/anysoftkeyboard/datacollection/BatteryInfo.java
@@ -1,19 +1,40 @@
 package com.anysoftkeyboard.datacollection;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
 public class BatteryInfo {
-   private boolean isCharging;
-   private int batteryLevel;
-   private int batteryStatus;
+
+    private boolean isCharging;
+    private float batteryPercentage;
+    private int batterLevel;
+    private int batteryStatus;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public BatteryInfo( BatteryManager batteryManager) {
+    public BatteryInfo(BatteryManager batteryManager) {
         this.isCharging = batteryManager.isCharging();
-        this.batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+//        this.batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
         this.batteryStatus = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_STATUS);
+    }
+
+    public float getBatteryPercentage() {
+        return batteryPercentage;
+    }
+
+    public void setBatteryPercentage(float batteryPercetage) {
+        this.batteryPercentage = batteryPercetage;
+    }
+
+    public int getBatterLevel() {
+        return batterLevel;
+    }
+
+    public void setBatterLevel(int batterLevel) {
+        this.batterLevel = batterLevel;
     }
 
     public boolean isCharging() {
@@ -22,14 +43,6 @@ public class BatteryInfo {
 
     public void setCharging(boolean charging) {
         isCharging = charging;
-    }
-
-    public int getBatteryLevel() {
-        return batteryLevel;
-    }
-
-    public void setBatteryLevel(int batteryLevel) {
-        this.batteryLevel = batteryLevel;
     }
 
     public int getBatteryStatus() {
@@ -44,8 +57,24 @@ public class BatteryInfo {
     public String toString() {
         return "BatteryInfo{" +
                 "isCharging=" + isCharging +
-                ", batteryLevel=" + batteryLevel +
+                ", batteryPercetage=" + batteryPercentage +
+                ", batterLevel=" + batterLevel +
                 ", batteryStatus=" + batteryStatus +
                 '}';
+    }
+
+    public static float getBatteryPercentage(Context context) {
+        IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, iFilter);
+
+        int level = batteryStatus != null ? batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) : -1;
+        int scale = batteryStatus != null ? batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1) : -1;
+        return (level / (float) scale);
+    }
+
+    public static int getBatteryLevel(Context context) {
+        IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, iFilter);
+        return batteryStatus != null ? batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) : -1;
     }
 }
